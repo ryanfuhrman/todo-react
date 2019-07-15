9692d773848f26850da27c41642045624624e165
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-07-15 16:19:23 -0400

    Added update form to render

--- src/Todo.js
+++ src/Todo.js
@@ -3,27 +3,63 @@ import React, { Component } from "react";
 export default class Todo extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      edit: false,
+      task: this.props.task,
+    };
+
     this.handleRemove = this.handleRemove.bind(this);
-    this.handleRemove = this.handleRemove.bind(this);
+    this.toggleEdit = this.toggleEdit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
 
   handleRemove() {
-    // clickFunction()
     this.props.removeTodo(this.props.id);
   }
 
-  handleEdit() {
-    this.props.editTodo(this.props.id);
+  toggleEdit(todo, id) {
+    this.setState({ edit: true });
+  }
+
+  handleUpdate(e) {
+    e.preventDefault();
+    this.props.updateTodo(this.props.id, this.state.task);
+    this.setState({ edit: false });
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   }
 
   render() {
-    const { todo, id } = this.props;
-    return (
-      <div>
-        <p key={id}>{todo}</p>
-        <button onClick={this.handleEdit}>edit</button>
-        <button onClick={this.handleRemove}>x</button>
-      </div>
-    );
+    let display;
+
+    if (this.state.edit) {
+      display = (
+        <form onSubmit={this.handleUpdate}>
+          <label htmlFor="task" />
+          <input
+            name="task"
+            type="text"
+            value={this.state.task}
+            onChange={this.handleChange}
+          />
+          <button>Update Todo</button>
+        </form>
+      );
+    } else {
+      display = (
+        <div>
+          <p>{this.props.task}</p>
+          <button onClick={this.toggleEdit}>edit</button>
+          <button onClick={this.handleRemove}>x</button>
+        </div>
+      );
+    }
+
+    return <li>{display}</li>;
   }
 }
