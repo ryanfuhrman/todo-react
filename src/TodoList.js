7003ce05c606560a16fc9fda4d8b538e04fcec87
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-07-16 14:38:29 -0400

    Added toggleCompletion function and passed as prop to Todo component

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,12 +7,29 @@ export default class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [],
+      todos: [
+        {
+          task: "walk the dog",
+          id: "anfk43lqbfa4kjfa4labfaj4f4fna4",
+          completed: false,
+        },
+        {
+          task: "take out the trash",
+          id: "aasdfaffawa4kjfa4labfaj4f4fna4",
+          completed: false,
+        },
+        {
+          task: "finish project",
+          id: "anfk52t4gb6balabfaj4f4fna4",
+          completed: false,
+        },
+      ],
     };
 
     this.addTodo = this.addTodo.bind(this);
     this.removeTodo = this.removeTodo.bind(this);
     this.updateTodo = this.updateTodo.bind(this);
+    this.toggleCompleted = this.toggleCompleted.bind(this);
   }
 
   addTodo(newTodo) {
@@ -35,6 +52,14 @@ export default class TodoList extends Component {
     }));
   }
 
+  toggleCompleted(id) {
+    this.setState(st => ({
+      todos: st.todos.map(todo => {
+        return todo.id === id ? { ...todo, completed: !todo.completed } : todo;
+      }),
+    }));
+  }
+
   render() {
     return (
       <div className="TodoList">
@@ -43,13 +68,15 @@ export default class TodoList extends Component {
           <NewTodoForm addTodo={this.addTodo} />
         </div>
         <ul className="TodoList-list">
-          {this.state.todos.map(({ task, id }) => (
+          {this.state.todos.map(({ task, id, completed }) => (
             <Todo
               task={task}
               key={id}
               id={id}
+              completed={completed}
               removeTodo={this.removeTodo}
               updateTodo={this.updateTodo}
+              toggleCompleted={this.toggleCompleted}
             />
           ))}
         </ul>
