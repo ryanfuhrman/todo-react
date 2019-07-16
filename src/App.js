4af6c6484dc5e902fdd4702c98b09977ccbac8d0
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-07-16 16:32:39 -0400

    Added fontawesome library and icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
 import TodoList from "./TodoList";
 import "./App.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faTrash,
+  faTrashAlt,
+  faEdit,
+  faSquare,
+  faCheckSquare,
+} from "@fortawesome/free-solid-svg-icons";
+// import { faTrashAlt, faEdit } from "@fortawesome/fontawesome-free";
+
+library.add(faTrash, faTrashAlt, faEdit, faSquare, faCheckSquare);
 
 function App() {
   return (
